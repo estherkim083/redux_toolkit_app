dedcf3f161a1b4e0da9dfcb7facb122c510de6a7
estherkim083 <estherkim083@gmail.com>
2023-01-20 09:31:01 +0900

    chap6

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import { fetchUsers } from "./features/users/usersSlice";
-import { fetchPosts } from "./features/posts/postsSlice";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
+import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
-  </Provider>
+  <React.StrictMode>
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
+  </React.StrictMode>
 );
