769f305c9fc6c615a26a41b34ee7c9c513a74ce0
estherkim083 <estherkim083@gmail.com>
2023-01-20 13:17:22 +0900

    chap7

---
+++ src/features/posts/PostsList.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { selectPostIds } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from "./postsSlice";
+
+const PostsList = () => {
+  const orderedPostIds = useSelector(selectPostIds);
+  const { isLoading, isSuccess, isError, error } = useGetPostsQuery();
+
+  let content;
+  if (isLoading) {
+    content = <p>"Loading..."</p>;
+  } else if (isSuccess) {
+    content = orderedPostIds.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
+    ));
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+
+  return <section>{content}</section>;
+};
+export default PostsList;
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { extendedApiSlice } from "./features/posts/postsSlice";
+import { usersApiSlice } from "./features/users/usersSlice";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { apiSlice } from "./features/api/apiSlice";
+store.dispatch(extendedApiSlice.endpoints.getPosts.initiate());
+store.dispatch(usersApiSlice.endpoints.getUsers.initiate());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ApiProvider api={apiSlice}>
-      <App />
-    </ApiProvider>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
